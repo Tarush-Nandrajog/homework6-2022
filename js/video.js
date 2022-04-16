e5443ea8f81b984380701a4169c38d0403bf5b33
Tarush-Nandrajog <97184684+Tarush-Nandrajog@users.noreply.github.com>
2022-04-16 13:18:09 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -4,13 +4,13 @@ window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 	video.autoplay = false;
 	video.loop = false;
-	console.log("the current volume is " + video.volume)
-	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play()
+	console.log("the current volume is " + video.volume)
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
